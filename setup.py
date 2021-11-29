187528b029ce9ce4697b12f3aa9c5ec1a9ac7bba
CSynodinos <chris_sinodinos@outlook.com>
2021-11-29 18:15:39 +0000

    none

--- setup.py
+++ setup.py
@@ -50,5 +50,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     install_requires=load_requirements("requirements.txt"),
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
     python_requires='>=3.6'
 )
