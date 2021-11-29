dc4bbb22f988d60fb06cff7db6ee8d28c8f33e77
Christos Synodinos <chris_sinodinos@outlook.com>
2021-11-29 16:40:43 +0000

    None

--- tests/unit.py
+++ tests/unit.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python3
 
-import sys
 import os
-
-current = os.path.dirname(os.path.realpath(__file__))
-parent = os.path.dirname(current)
-sys.path.append(parent)
-
 from converters import txtconvert, imgconvert, sheetconvert
 
 class test():
